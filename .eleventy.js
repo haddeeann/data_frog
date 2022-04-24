acb2cc9a98145e2e8d238bbfceb064a6fbcc7873
HaddeeAnn <haddeeann@HaddeeAnns-MBP.attlocal.net>
2022-04-24 19:29:56 -0400

    added a little bit of style

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+    // Output directory: _site
+
+    // Copy `img/` to `_site/img`
+    eleventyConfig.addPassthroughCopy("img");
+
+    // Copy `css/` to `_site/css/`
+    // Keeps the same directory structure.
+    eleventyConfig.addPassthroughCopy("css/");
+
+    // Copy `css/fonts/` to `_site/css/fonts`
+    // Keeps the same directory structure.
+    eleventyConfig.addPassthroughCopy("css/fonts");
+
+    // Copy any .jpg file to `_site`, via Glob pattern
+    // Keeps the same directory structure.
+    eleventyConfig.addPassthroughCopy("**/*.jpg");
+};
