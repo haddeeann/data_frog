174ffac206d13d410f0fccb17ca9b9c06b8d8979
HaddeeAndd <haddeeann@gmail.com>
2023-12-19 13:22:28 -0500

    update the image so we have a little frog

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./static/");
     return {
         dir: {
             input: "src",
